141364b98d2eac7fcdd1524b6c76d3ec34f0050c
TamaWilson <wilson_tama@hotmail.com>
2017-06-05 22:55:12 -0300

    Ajustes

---
+++ README.md
@@ -0,0 +1,22 @@
+#UTILZIADO
+
+- Python 3.5
+- Django 1.10 - Framework Python para web
+- HTML, Jquery, CSS Bootstrap
+- PILLOW - Fork da Pil para python 3.X
+
+
+#Arquivos importantes
+
+- VIEWS.PY (photo/views.py)
+- URLS.py (fotoxop/urls.py e photo/ulrs.py)
+- Tudo dentro da pasta /photo/templates/photo/
+
+#+ - como o Django funciona
+
+Ao abrir uma URL é gerado uma requisição para o servidor, essa requisição é tratada pelos arquivos URLS.py, cada URL direciona para uma função diferente do VIEWS.py
+Cada função do VIEWS.py retorna um template com um dicionario de contextos contendo as informações a serem renderizadas no navegador
+
+Cada botão do sistema possui um listener está atrelado a um listener do JQuery, para ser chamado no "On CLick", o botão invoca o ajax e manda a requisição necessária para o servidor tratar
+
+* Dicionário em python nada mais é que um Hashmap com um outro nome
--- photo/views.py
+++ photo/views.py
@@ -6,82 +6,126 @@ import PIL.ImageOps
 from PIL import ImageEnhance
 
 
-
-
-
+#Função chamada ao requisitar a view do index
 def index(request):
+
+    #rendezira o template da index
     return render(request, 'photo/index.html')
 
 
+#Função chamada ao requisitar a view de uploar
 def upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
+
+    #checa que se a requisição veio via POST e se o arquivo existe
+    if request.method == 'POST' and request.FILES.get('myfile', False): #se não houver a chave 'myfile' o valor padrão é False
+
+        #instancia uma variavel com o conteúdo do dicionario de arquivos
         myfile = request.FILES['myfile']
+
+        print(type(myfile))
+
+        #instancia um objeto para manipular o armazenamento de arquivos
         fs = FileSystemStorage()
 
+        #salva o arquivo enviado com o nome original
+        filename = fs.save(myfile.name, myfile)
 
-        filename = fs.save(myfile.name, myfile,)
+        #captura a url do arquivo
         uploaded_file_url = fs.url(filename)
 
+        #abre o arquivo com PIL passando o caminho absoluto para o arquivo no servidor
         image = Image.open(fs.base_location + "\\" + filename)
 
+        #utiliza a propriedade size para retornar a altura e largura da imagem
+        #image.size retorna uma tupla
         width, height = image.size
 
+        #cria um dicionário com as variaveis de contexto
         context = {
-            'uploaded_file_url': uploaded_file_url,
-            'uploaded_filename' : filename,
-            'width' : width,
-            'height' : height
+            'uploaded_file_url': uploaded_file_url, #caminho para o arquivo
+            'uploaded_filename' : filename, #nome do arquivo
+            'width' : width, #largura
+            'height' : height #altura
         }
 
-        return render(request, 'photo/canvas.html', context)
-    return redirect('/fotoxop')
+        return render(request, 'photo/canvas.html', context) #renderiza o template com o contexto
+    return redirect('/fotoxop') #Caso a condicao do IF seja falsa, retorna para a página inicial
 
-@csrf_exempt
+@csrf_exempt #ignora a exigência do csrf cookie - apenas para fins didáticos
 def rotate(request):
+    #Checa se a requisição veio via POST
     if request.method == 'POST':
+
+        #Lê o filename do dicionário POST
         myfile = request.POST['filename']
+
+        #Lê o ângulo do dicionário POST
         angle = int(request.POST['angle'])
 
-
+        #Instancia do objeto de manipulação do armazenamento
         fs = FileSystemStorage()
+
+        #Monta o caminho absoluto para o arquivo
         filename = fs.base_location + "\\" + myfile
 
+        #Abre a imagem com PILLOW
         image = Image.open(filename)
 
+        #Rotaciona a imagem com o ângulo indicado
+        #expand=true - o tamanho é ajustado para as novas dimensões após a rotação
+        #caso expand=false os valores de altura x largura permanecem inalterados
         rotated = image.rotate(angle, expand=True)
 
+        #salva a imagem rotacionada por cima da original
         rotated.save(filename)
 
+        #monta a url para o arquivo
         uploaded_file_url = fs.url(myfile)
+
+        #retorna com a renderização do template de atualização e o filename do arquivo
         return render_to_response('photo/updated.html',  {
             'uploaded_file_url': uploaded_file_url
         })
-    return redirect('/fotoxop')
+    return redirect('/fotoxop') #Se a requisição não veio via POST retorna redirecionara para a index
 
-@csrf_exempt
+@csrf_exempt #ignora a exigência do csrf cookie - apenas para fins didáticos
 def invert(request):
+    # Checa se a requisição veio via POST
     if request.method == 'POST':
+        # Lê o filename do dicionário POST
         myfile = request.POST['filename']
 
+        # Instancia do objeto de manipulação do armazenamento
         fs = FileSystemStorage()
+
+        # Monta o caminho absoluto para o arquivo
         filename = fs.base_location + "\\" + myfile
 
+        # Abre a imagem com PILLOW
         image = Image.open(filename)
+
+        #Inverte as cores
         inverted = PIL.ImageOps.invert(image)
 
+        #Salva a imagem invertida por cima da original
         inverted.save(filename)
 
+        # monta a url para o arquivo
         uploaded_file_url = fs.url(myfile)
 
+        # retorna com a renderização do template de atualização e o filename do arquivo
         return render_to_response('photo/updated.html',  {
             'uploaded_file_url': uploaded_file_url
         })
-    return redirect('/fotoxop')
+    return redirect('/fotoxop') #Se a requisição não veio via POST retorna redirecionara para a index
 
 @csrf_exempt
 def contrast(request):
     if request.method == 'POST':
         myfile = request.POST['filename']
+
+        # Lê o level passado pelo usuário, divide por a escala em HTML varia de 0 a 100.
+        # A posição padrão do level é no 50 no html é 50, divimos por esse valo para o Pillow entender como 1.0
         level = float(request.POST['level'])/50
 
 
@@ -90,9 +134,13 @@ def contrast(request):
 
         image = Image.open(filename)
 
-        enh = ImageEnhance.Contrast(image)
+        #Instanciando um objeto da classe ImageEnhance.Constrast, passando a imagem a ser manipulada
+        enh = ImageEnhance.Contrast(image) #0 = imagem sem contraste, 1.0= Nível Padrão (Nada alterado), 2.0 = Contraste máximo permitido
+
+        #Aplica o contraste com o level indicado pelo usuário
         contrast = enh.enhance(level)
 
+        #Salva a imagem alterada por cima da original
         contrast.save(filename)
 
         uploaded_file_url = fs.url(myfile)
@@ -115,10 +163,11 @@ def bright(request):
 
         image = Image.open(filename)
 
+        #Objeto do timpo ImageEnhance.Brightness - BRILHO
         enh = ImageEnhance.Brightness(image)
-        contrast = enh.enhance(level)
+        brightness = enh.enhance(level) #0 nenhum brilho, 1.0 = Nenhuma alteração, 2.0 = brilho máximo permitido
 
-        contrast.save(filename)
+        brightness.save(filename)
 
         uploaded_file_url = fs.url(myfile)
 
@@ -142,10 +191,11 @@ def color(request):
 
         image = Image.open(filename)
 
+        #Objeto de manipulação da cor
         enh = ImageEnhance.Color(image)
-        contrast = enh.enhance(level)
+        color = enh.enhance(level) #0 Sem cor = Cinza, 1.0 - não altera, 2.0 cor no máximo permitido
 
-        contrast.save(filename)
+        color.save(filename)
 
         uploaded_file_url = fs.url(myfile)
 
@@ -164,6 +214,7 @@ def grey(request):
 
         image = Image.open(filename)
 
+        #Objeto recebendo a imagem em escala de cinza
         grScale = PIL.ImageOps.grayscale(image)
 
         grScale.save(filename)
@@ -179,14 +230,15 @@ def grey(request):
 def resize(request):
     if request.method == 'POST':
         myfile = request.POST['filename']
-        width = int(request.POST['width'])
-        height = int(request.POST['height'])
+        width = int(request.POST['width']) #captura do dicionário POST a largura
+        height = int(request.POST['height']) #captura do dicionário POST a altura
 
         fs = FileSystemStorage()
         filename = fs.base_location + "\\" + myfile
 
         image = Image.open(filename)
 
+        #Image.Image.resize = redimensionar a imagem com os valores coloados pelo usuário
         resized = Image.Image.resize(image,(width,height))
 
         resized.save(filename)
@@ -203,23 +255,35 @@ def resize(request):
 def channel(request):
     if request.method == 'POST':
         myfile = request.POST['filename']
-        value = request.POST['channel']
-
+        value = request.POST['channel'] #Verifica o canal a ser exibido
+        #R - Red
+        #G - Green
+        #B - Blue
+        #RGB - Recuperado no javascript
 
         fs = FileSystemStorage()
         filename = fs.base_location + "\\" + myfile
+
+        #monta um novo filename
         newFilename = fs.base_location + "\\CHANNEL" + myfile
+
         image = Image.open(filename)
 
+        #Cria um dicionario com as chaves de cada cor
         colors = { 'R' : 0, 'B': 0, 'G': 0 }
 
+        #Image.Image.Split retorna uma tupla com 3 imagens, uma para cada canal
+        #As imagens são atribuidas cada uma para uma chave do dicionário Colors
         colors['R'], colors['G'], colors['B'] = Image.Image.split(image)
 
-        splited = colors[value]
+        #Splited recebe um valor de Colors de acordo com o valor atribuido para Value (R, G ou B)
+        splited = colors[value] #colors['B'] = retorna o canal Blue
 
-
+        #salva uma nova imagem
         splited.save(newFilename)
 
+
+        #retorna a url do novo arquivo
         uploaded_file_url = fs.url("CHANNEL" + myfile)
 
         return render_to_response('photo/updated.html', {
@@ -236,6 +300,8 @@ def mirror(request):
         filename = fs.base_location + "\\" + myfile
 
         image = Image.open(filename)
+
+        #Aplica a inversão na imagem
         mirrored = PIL.ImageOps.mirror(image)
 
         mirrored.save(filename)
